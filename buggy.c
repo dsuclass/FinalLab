f51a48290ce4d2766074861770134822358e3463
dsuclass <44726861+dsuclass@users.noreply.github.com>
2020-11-12 21:10:28 -0800

    Create buggy.c

---
+++ buggy.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+//FIND AND FIX THE MISTAKES IN THIS CODE
+
+char password[80);
+
+print("Create a password: "]:
+scanf("%s", password);
+
+integer passwordlength;
+passwordlength = strlen(password);
+printf("Your password is %s and %i characters long", password, passwordlength) 
